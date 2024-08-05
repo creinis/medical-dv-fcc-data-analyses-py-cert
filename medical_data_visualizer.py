7638e3b4e1c705b3a145eaad39e4cf9725bb38b5
creinis <115181527+creinis@users.noreply.github.com>
2024-08-05 15:29:23 +0200

    feat: Use the value 0 for NOT overweight and the value 1 for overweight.     Normalize the data by making 0 always good and 1 always bad. If the value of cholesterol or gluc is 1, make the value 0. If the value is more than 1, make the value 1

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -9,5 +9,11 @@ df = pd.read_csv('medical_examination.csv')
 # Add 'overweight' column
 df['overweight'] = (df['weight'] / ((df['height'] / 100)**2) > 25).astype(int)
 
+# Normalize data by making 0 always good and 1 always bad.
+# If the value of 'cholesterol' or 'gluc' is 1, make the value 0.
+# If the value is more than 1, make the value 1.
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
+
 
 
